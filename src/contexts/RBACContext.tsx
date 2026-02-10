259d1dce1045ab490f892e6dc6d911dae26fe1a6
eljie <eljieg.dev@gmail.com>
2026-02-10 23:32:59 +0800

    additional lint fixes

--- src/contexts/RBACContext.tsx
+++ src/contexts/RBACContext.tsx
@@ -53,6 +53,7 @@ export function RBACProvider({ children }: { children: ReactNode }) {
   );
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export function useRBAC() {
   const context = useContext(RBACContext);
   if (!context) {
